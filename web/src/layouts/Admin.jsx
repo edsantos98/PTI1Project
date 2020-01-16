9ccd526a9d857c510118636ca9ac5939ef7674e3
Leandro Alves <43756835+AzKeryon@users.noreply.github.com>
2020-01-16 22:24:01 +0000

    resolving warnings

--- web/src/layouts/Admin.jsx
+++ web/src/layouts/Admin.jsx
@@ -22,7 +22,6 @@ import { Container } from "reactstrap";
 // core components
 import AdminNavbar from "components/Navbars/AdminNavbar.jsx";
 import AdminFooter from "components/Footers/AdminFooter.jsx";
-import Sidebar from "components/Sidebar/Sidebar.jsx";
 
 import routes from "routes.js";
 
