bfe14a414523a629fe0c63fd6bc39f6b751f0459
Eduardo Santos <a82350@alunos.uminho.pt>
2020-01-16 21:45:21 +0000

    Update index.js

--- node.js/index.js
+++ node.js/index.js
@@ -7,7 +7,7 @@ const app = express();
 const timeout = 60000000;
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://35.246.98.225:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   //res.header("Access-Control-Allow-Origin", "http://azkeryon.duckdns.org:3000");
